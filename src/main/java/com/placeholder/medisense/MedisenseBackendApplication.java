f64e0b6f4c2be0a465d6425102187afb0ec3562b
Ajinkya Gore <ajinkyagore@Ajinkyas-MacBook-Air.local>
2023-07-15 18:14:14 +0530

    Initial commit

---
+++ src/main/java/com/placeholder/medisense/MedisenseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.placeholder.medisense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedisenseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedisenseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/placeholder/medisense/MedisenseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.placeholder.medisense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedisenseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
